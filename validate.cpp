e8aa79c46d2f5d34f4eab4a8a075194d75bc4f28
claudio <claudio.landi24@gmail.com>
2023-11-23 04:46:58 +0000

    definitions of functions in validate.h moved to validate.cpp

--- validate.cpp
+++ validate.cpp
@@ -1,6 +1,52 @@
 #include "validate.h"
 #include <QTextStream>
 
+QPair<bool, QString> Validate::getValidatedString(const QString& string) {
+	Q_UNUSED(string);
+	return {false, QString()};
+};
+
+QPair<bool, int> Validate::getValidatedInt(const QString& string) {
+	Q_UNUSED(string);
+	return {false, 0};
+};
+
+QPair<bool, QString> ValidateGeneralAlphaNum::getValidatedString(const QString& string) {
+	bool ok = isAlphanumeric(string, 100);
+	if (!ok) {
+		return {false, QString()};
+	}
+	return {true, string};
+}
+
+QPair<bool, int> ValidatePositiveOrZeroInteger::getValidatedInt(const QString& string) {
+	bool ok;
+	int  n = string.toInt(&ok);
+	if (!ok) {
+		return {false, 0};
+	}
+	if (n < 0) {
+		return {false, 0};
+	}
+	return {true, n};
+}
+
+QPair<bool, int> ValidateType::getValidatedInt(const QString& string) {
+	int typeId = getTypeId(string);
+	if (!typeId) {
+		return {false, 0};
+	}
+	return {true, typeId};
+}
+
+QPair<bool, int> ValidateLocation::getValidatedInt(const QString& string) {
+	int locationId = getLocationId(string);
+	if (!locationId) {
+		return {false, 0};
+	}
+	return {true, locationId};
+}
+
 QString getValidatedString(const QString& requestMsg, unique_ptr<Validate> validate) {
 	while (true) {
 		QTextStream(stdout) << requestMsg << Qt::endl;
--- validate.h
+++ validate.h
@@ -1,69 +1,34 @@
 #ifndef VALIDATE_H
 #define VALIDATE_H
 
-#include <QString>
-#include <QPair>
 #include "utilityfunctions.h"
+#include <QPair>
+#include <QString>
 
 class Validate {
       public:
-	virtual QPair<bool, QString> getValidatedString(const QString& string) {
-		Q_UNUSED(string);
-		return {false, QString()};
-	};
-	virtual QPair<bool, int> getValidatedInt(const QString& string) {
-		Q_UNUSED(string);
-		return {false, 0};
-	};
+	virtual QPair<bool, QString> getValidatedString(const QString& string);
+	virtual QPair<bool, int>     getValidatedInt(const QString& string);
 	virtual ~Validate() = default;
 };
 
 class ValidateGeneralAlphaNum : public Validate {
-	QPair<bool, QString> getValidatedString(const QString& string) override {
-		bool ok = isAlphanumeric(string, 100);
-		if (!ok) {
-			return {false, QString()};
-		}
-		return {true, string};
-	}
+	QPair<bool, QString> getValidatedString(const QString& string) override;
 };
 
 class ValidatePositiveOrZeroInteger : public Validate {
-	QPair<bool, int> getValidatedInt(const QString& string) override {
-		bool ok;
-		int  n = string.toInt(&ok);
-		if (!ok) {
-			return {false, 0};
-		}
-		if (n < 0) {
-			return {false, 0};
-		}
-		return {true, n};
-	}
+	QPair<bool, int> getValidatedInt(const QString& string) override;
 };
 
 class ValidateType : public Validate {
-	QPair<bool, int> getValidatedInt(const QString& string) override {
-		int typeId = getTypeId(string);
-		if (!typeId) {
-			return {false, 0};
-		}
-		return {true, typeId};
-	}
+	QPair<bool, int> getValidatedInt(const QString& string) override;
 };
 
 class ValidateLocation : public Validate {
-	QPair<bool, int> getValidatedInt(const QString& string) override {
-		int locationId = getLocationId(string);
-		if (!locationId) {
-			return {false, 0};
-		}
-		return {true, locationId};
-	}
+	QPair<bool, int> getValidatedInt(const QString& string) override;
 };
 
 QString getValidatedString(const QString& requestMsg, unique_ptr<Validate> validate);
 int     getValidatedInt(const QString& requestMsg, unique_ptr<Validate> validate);
-
 
 #endif // VALIDATE_H
